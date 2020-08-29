306eda00952b982275b91c0ccfda03e6d4f3ff3a
ahnjuny0ung <67837091+ahnjuny0ung@users.noreply.github.com>
2020-08-30 00:27:08 +0900

    add search_app, paginator, design

--- perfectcushion/urls.py
+++ perfectcushion/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('shop/', include('shop.urls')),
+    path('search/', include('search.urls')),
 ]
 
 if settings.DEBUG:
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse    
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+# Paginator는 여러 개의 아이템을 페이지 단위로 관리할 수 있게 도와준다.
 
 # Create your views here.
 def index(request):
@@ -9,13 +11,26 @@ def index(request):
 
 def allProdCat(request, c_slug=None):
     c_page = None
-    products = None
+    products_list = None
 
     if c_slug != None:
         c_page = get_object_or_404(Category, slug=c_slug)
-        products = Product.objects.filter(category=c_page, available=True)
+        products_list = Product.objects.filter(category=c_page, available=True)
     else:
-        products = Product.objects.all().filter(available=True)
+        products_list = Product.objects.all().filter(available=True)
+
+    # Pagintor Code
+    paginator = Paginator(products_list, 1)
+    try:
+        page = int(request.GET.get('page','3'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+
     return render(request, 'shop/category.html', {'category':c_page, 'products':products})
 
 def ProdCatDetail(request, c_slug, product_slug):
